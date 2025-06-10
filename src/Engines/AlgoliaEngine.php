677a41c8d04861ce5d3398849f3933bba74132fa
RAMON ARAUJO OLIVEIRA <ramonaololiveira@gmail.com>
2025-06-09 23:19:15 -0300

    Vesion: 007.000.000.000

--- src/Engines/AlgoliaEngine.php
+++ src/Engines/AlgoliaEngine.php
@@ -3,22 +3,22 @@
 namespace Laravel\Scout\Engines;
 
 use Laravel\Scout\Builder;
-use AlgoliaSearch\Client as Algolia;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Algolia\AlgoliaSearch\SearchClient as Algolia;
 
 class AlgoliaEngine extends Engine
 {
     /**
      * The Algolia client.
      *
-     * @var \AlgoliaSearch\Client
+     * @var \Algolia\AlgoliaSearch\SearchClient
      */
     protected $algolia;
 
     /**
      * Create a new engine instance.
      *
-     * @param  \AlgoliaSearch\Client  $algolia
+     * @param  \Algolia\AlgoliaSearch\SearchClient  $algolia
      * @return void
      */
     public function __construct(Algolia $algolia)
@@ -30,7 +30,7 @@ class AlgoliaEngine extends Engine
      * Update the given model in the index.
      *
      * @param  \Illuminate\Database\Eloquent\Collection  $models
-     * @throws \AlgoliaSearch\AlgoliaException
+     * @throws \Algolia\AlgoliaSearch\Exceptions\AlgoliaException
      * @return void
      */
     public function update($models)
@@ -58,7 +58,7 @@ class AlgoliaEngine extends Engine
         })->filter()->values()->all();
 
         if (! empty($objects)) {
-            $index->addObjects($objects);
+            $index->saveObjects($objects);
         }
     }
 
@@ -203,7 +203,7 @@ class AlgoliaEngine extends Engine
     {
         $index = $this->algolia->initIndex($model->searchableAs());
 
-        $index->clearIndex();
+        $index->clearObjects();
     }
 
     /**
